69d5ec4557823da964e21954df9b8ed002a983a8
Nanna Feltendahl <nanna@nannafeltendahl.com>
2024-04-23 19:18:40 +0100

    javascript added to site

--- index.js
+++ index.js
@@ -16,4 +16,81 @@ $(document).ready(function(){
     });
 });
 
+//detecting button press
+
+function log(message) {
+    if (console && console.log) {
+        console.log(message)
+    }
+}
+
+const numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        let buttonInnerHtml = this.innerHTML.toLowerCase();
+
+        // log(buttonInnerHtml)
+
+        makesound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+
+    });
+}
+
+//detecting keyboard press
+document.addEventListener("keydown", function (event) {
+    makesound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+function makesound(key) {
+
+    switch (key) {
+        case "a":
+            var crash = new Audio("sounds/correctSound")
+            crash.play();
+            break;
+
+        case "b":
+            var kick = new Audio("sounds/incorrectSound")
+            kick.play();
+            break;
+
+        default:
+            console.log("Unhandled key: " + key);
+    }
+
+
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+
+    if (activeButton){
+        activeButton.classList.add("pressed");
+
+        setTimeout(function () {
+            activeButton.classList.remove("pressed");
+        }, 100);
+    }
+}
+
+const acc = document.getElementsByClassName("accordion");
+for ( let i = 0; i < acc.length; i++) {
+    acc [i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+
+    });
+}
+
 
